8ab5829bb5eee470c7bb080ec1f0abadeadf0cf8
Mohamedlemin <med3744lemin@gmail.com>
2022-05-03 18:25:25 +0000

    add Hello mohamed-lemin

--- src/App.js
+++ src/App.js
@@ -7,17 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello
-          Edit <code>src/App.js</code> and save to reload.
+          Hello <code>Mohamed lemin </code> Happy coding!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
